183d6916ecc32b9ce5bcbeed08635a2f9771f486
Oditha Amarasinghe <odi@Odithas-MacBook-Pro.local>
2022-10-11 14:48:21 -0400

    new updates

--- editorScript.py
+++ editorScript.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-from webbrowser import get
 from aubio import tempo, source
 from numpy import median, diff
-from moviepy.editor import * 
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.VideoClip import ImageClip
+from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from natsort import natsorted
 import glob
+import sys
 
 def get_file_bpm(path, params=None):
     if params is None:
         params = {}
-    # default:
     samplerate, win_s, hop_s = 44100, 1024, 512
 
     s = source(path, samplerate, hop_s)
 
     samplerate = s.samplerate
     o = tempo("specdiff", win_s, hop_s, samplerate)
-    # List of beats, in samples
     beats = []
-    # Total number of frames read
     total_frames = 0
 
     while True:
@@ -27,14 +26,24 @@ def get_file_bpm(path, params=None):
         if is_beat:
             this_beat = o.get_last_s()
             beats.append(this_beat)
-            #if o.get_confidence() > .2 and len(beats) > 2.:
-            #    break
         total_frames += read
         if read < hop_s:
             break
 
     bpms = 60. / diff(beats)        
     return round(median(bpms))
+
+print("\n")
+print("##############################################################\n")
+print("Welcome to AutoEditor!\n")
+print("Make sure your JPEGS are in the current directory!\n")
+print("Also make sure your audio is a .wav file\n")
+print("Enter q if you would like to quit or any other key to progress\n")
+print("##############################################################\n")
+
+x = input()
+if x == "q":
+    sys.exit()
 
 audio_file = input("Please give the path of your audio file:\n")
 bpm = get_file_bpm(audio_file, None)
@@ -45,6 +54,7 @@ gif_name = 'pic'
 fps = 24
 
 file_list = glob.glob('*.JPEG')  # Get all the pngs in the current directory
+print(file_list)
 file_list_sorted = natsorted(file_list,reverse=False)  # Sort the images
 
 time = int(input("How fast would you like your clips?\n" + "i.e 1 => normal speed, 2 => double speed, etc.\n"))
